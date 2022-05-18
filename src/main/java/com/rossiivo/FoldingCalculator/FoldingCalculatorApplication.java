a49a9c58acbe2ced7053a8ce66c2f83e837fdc55
ivo <rossiivomartin@gmail.com>
2022-05-18 20:32:13 -0300

    estructura de proyecto

---
+++ src/main/java/com/rossiivo/FoldingCalculator/FoldingCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.rossiivo.FoldingCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoldingCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoldingCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rossiivo/FoldingCalculator/FoldingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rossiivo.FoldingCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoldingCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
