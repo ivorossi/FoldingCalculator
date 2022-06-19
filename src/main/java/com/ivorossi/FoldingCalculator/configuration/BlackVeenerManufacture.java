e735c9f8a838ce2a56764625ba6b3ca8e8550036
Giancarlo <g.galvarini@alumno.um.edu.ar>
2022-06-19 12:51:19 -0300

    update

---
+++ src/main/java/com/ivorossi/FoldingCalculator/configuration/BlackVeenerManufacture.java
@@ -0,0 +1,23 @@
+package com.ivorossi.FoldingCalculator.configuration;
+
+
+import com.ivorossi.FoldingCalculator.model.PriceBlackVeener;
+import com.ivorossi.FoldingCalculator.repository.PriceVeenerBlackRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BlackVeenerManufacture {
+
+    @Bean
+    public CommandLineRunner autoSaveManufacture(PriceVeenerBlackRepository priceVeenerBlackRepository) {
+        return args -> {
+            priceVeenerBlackRepository.save(new PriceBlackVeener(1, "MMOP", 650f));
+            priceVeenerBlackRepository.save(new PriceBlackVeener(2, "MMOL", 550f));
+            priceVeenerBlackRepository.save(new PriceBlackVeener(3, "MOP", 140f));
+            priceVeenerBlackRepository.save(new PriceBlackVeener(4, "MOL", 90f));
+
+        };
+    }
+}
--- src/main/java/com/ivorossi/FoldingCalculator/service/BlackVeenerService.java
+++ src/main/java/com/ivorossi/FoldingCalculator/service/BlackVeenerService.java
@@ -8,38 +8,42 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BlackVeenerService {
+
     @Autowired
     PriceVeenerBlackRepository priceVeenerBlackRepository;
-    private float factorSelected(String veenerThickness){
-        switch (veenerThickness){
-            case "24":
-                return 0.7F;
-            case "22":
-                return 0.75F;
-            case "20":
-                return 0.8F;
-            case "18":
-                return 1F;
-            case"16":
-                return 1.3F;
-            case"14":
-                return 1.6F;
-            case"12":
-                return 2F;
-            case"1/8":
-                return 2.7F;
-            case "3/16":
-                return 4F;
-            default:
-                return 0F;
-        }
+
+    private float factorSelected(String veenerThickness) {
+        return switch (veenerThickness) {
+            case "24" -> 0.7F;
+            case "22" -> 0.75F;
+            case "20" -> 0.8F;
+            case "18" -> 0.9F;
+            case "16" -> 1.3F;
+            case "14" -> 1.6F;
+            case "12" -> 2F;
+            case "1/8" -> 2.7F;
+            case "3/16" -> 4F;
+            default -> 0F;
+        };
     }
 
-    public float blackVeenerPriceCalculator(BlackVeenerDTO blackVeenerDTO){
-        PriceBlackVeener priceBlackVeener = priceVeenerBlackRepository.findByManufacture(blackVeenerDTO.getTypeManufacture());
-        float area = blackVeenerDTO.getHigh() * blackVeenerDTO.getWidth();
-        float mass = (area / 100000)* this.factorSelected(blackVeenerDTO.getVeenerThickness());
-        return (mass * priceBlackVeener.getPrice()) * blackVeenerDTO.getAmount();
+    private float areaCalculator(float high, float width) {
+        return high * width;
+    }
 
+    private float massCalculator(float area, float factor) {
+        return (area / 100000) * factor;
+    }
+
+    public float blackVeenerPriceCalculator(BlackVeenerDTO blackVeenerDTO) {
+        float area, mass;
+
+        PriceBlackVeener priceBlackVeener = priceVeenerBlackRepository
+                .findByManufacture(blackVeenerDTO.getTypeManufacture());
+
+        area = areaCalculator(blackVeenerDTO.getHigh(), blackVeenerDTO.getWidth());
+        mass = massCalculator(area, factorSelected(blackVeenerDTO.getVeenerThickness()));
+
+        return (mass * priceBlackVeener.getPrice()) * blackVeenerDTO.getAmount();
     }
 }
